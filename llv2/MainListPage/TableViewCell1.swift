6805d4ee71fea7da14fda2f0de5f97e88cf879d4
AixinLin <imlinaixin@gmail.com>
2018-07-25 16:19:01 -0400

    闲置广告上传页面

---
+++ llv2/MainListPage/TableViewCell1.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell1.swift
+//  llv2
+//
+//  Created by 林蔼欣 on 2018-07-25.
+//  Copyright © 2018 Luna Cao. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1: UITableViewCell {
+
+    @IBOutlet weak var image1: UIImageView!
+
+    @IBOutlet weak var image2: UIImageView!
+    @IBOutlet weak var image3: UIImageView!
+    
+    @IBOutlet weak var headImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    
+}
