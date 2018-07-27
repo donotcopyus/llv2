cd24f27fc5d5ecaa8a23d5658de652c6b3737b61
AixinLin <imlinaixin@gmail.com>
2018-07-26 23:08:44 -0400

    exchange update

--- llv2/sendAd/exchangeVC.swift
+++ llv2/sendAd/exchangeVC.swift
@@ -10,103 +10,98 @@ import UIKit
 
 class exchangeVC: UIViewController {
 
+    
     var button = dropDownBtn()
     var b2 = dropDownBtn()
-    
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        button = dropDownBtn.init(frame: CGRect(x:30, y:55, width: 150, height: 40))
+        button = dropDownBtn.init(frame: CGRect(x:30, y:155, width: 150, height: 40))
         
-        button.setTitle("出币种", for: .normal)
+        button.setTitle("求币种", for: .normal)
         
         button.translatesAutoresizingMaskIntoConstraints = true
         
         self.view.addSubview(button)
         
-   
-        button.dropView.dropDownOptions = ["Toronto","London","Hamilton","Waterloo"]
-        
-        
+    
+        button.dropView.dropDownOptions = ["RMB","加币","美金","港币","日元","英镑"]
         
         //---------------------------------
-        b2 = dropDownBtn.init(frame: CGRect(x:210, y:55, width: 150, height: 40))
+        b2 = dropDownBtn.init(frame: CGRect(x:210, y:155, width: 150, height: 40))
         
-        b2.setTitle("求币种", for: .normal)
+        b2.setTitle("出币种", for: .normal)
         
         b2.translatesAutoresizingMaskIntoConstraints = true
         
-        b2.dropView.dropDownOptions = ["Toronto","London","Hamilton","Waterloo"]
+        b2.dropView.dropDownOptions = ["RMB","加币","美金","港币","日元","英镑"]
         
         self.view.addSubview(b2)
-        // Do any additional setup after loading the view.
-        
  
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//
-    
-    //lauren————————————————————————————————————————————————————————————
-    //button to go back to main
-  
-  
-        
+    }
+   
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
     
     
-
-
-
-
+    //lauren————————————————————————————————————————————————————————————
+    //button to go back to main
+    @IBOutlet weak var back: UIButton!
+    @IBAction func goback(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    @IBOutlet weak var addInf: UITextField!
+    
+   
 //select departure city
 
-
+//
 //protocol dropDownProtocol {
 //    func dropDownPressed(string: String)
 //}
 
-//class dropDownBtn: UIButton, dropDownProtocol {
-//
-//    func dropDownPressed(string: String){
-//        self.setTitle(string, for: .normal)
-//
-//        // print(self.currentTitle!)
-//
-//        self.dismissDropDown()
-//    }
-//
-//    //cannot use 'let' here, would cause error in the line 'dropView = dropDownView .....'
+class dropDownBtn: UIButton, dropDownProtocol {
+    
+    func dropDownPressed(string: String){
+        self.setTitle(string, for: .normal)
+        
+        // print(self.currentTitle!)
+        
+        self.dismissDropDown()
+    }
+    
+    //cannot use 'let' here, would cause error in the line 'dropView = dropDownView .....'
     var dropView = dropDownView()
-//
+    
     var height = NSLayoutConstraint()
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//
-//        self.backgroundColor = UIColor.darkGray
-//
-//        dropView = dropDownView.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
-//
-//        //for the protocol
-//        dropView.delegate = self
-//
-//        dropView.translatesAutoresizingMaskIntoConstraints = false
-//
-//    }
-
-   // override func didMoveToSuperview() {
-//        self.superview?.addSubview(dropView)
-//        //how you want the menu to show up
-//        self.superview?.bringSubview(toFront: dropView)
-//        dropView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-//        dropView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-//        dropView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
-//        height = dropView.heightAnchor.constraint(equalToConstant: 0)
- //   }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.backgroundColor = UIColor.darkGray
+        
+        dropView = dropDownView.init(frame: CGRect.init(x: 0, y: 0, width: 0, height: 0))
+        
+        //for the protocol
+        dropView.delegate = self
+        
+        dropView.translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+    override func didMoveToSuperview() {
+        self.superview?.addSubview(dropView)
+        //how you want the menu to show up
+        self.superview?.bringSubview(toFront: dropView)
+        dropView.topAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        dropView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        dropView.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        height = dropView.heightAnchor.constraint(equalToConstant: 0)
+    }
     
     var isOpen = false
     
@@ -172,62 +167,68 @@ class exchangeVC: UIViewController {
     }
 }
 
-//class dropDownView: UIView, UITableViewDelegate, UITableViewDataSource {
-//    var dropDownOptions = [String]()
-//
-//    var tableView = UITableView()
-//
-//    var delegate : dropDownProtocol!
-//
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//
-//        tableView.backgroundColor = UIColor.darkGray
-//        self.backgroundColor = UIColor.darkGray
-//
-//        tableView.delegate = self
-//        tableView.dataSource = self
-//
-//        tableView.translatesAutoresizingMaskIntoConstraints = false
-//
-//        self.addSubview(tableView)
-//
-//        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-//        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-//        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-//        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-//    }
-//
-//
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
-//
-//
-//
-//    func numberOfSections(in tableView: UITableView) -> Int {
-//        return 1
-//    }
-//
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return dropDownOptions.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = UITableViewCell()
-//
-//        cell.textLabel?.text = dropDownOptions[indexPath.row]
-//        cell.backgroundColor = UIColor.darkGray
-//
-//        return cell
-//    }
-//
-//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//
-//        self.delegate.dropDownPressed(string: dropDownOptions[indexPath.row])
-//        self.tableView.deselectRow(at: indexPath, animated: true)
-//
-//    }
-//
-//
-//}
+
+
+class dropDownView: UIView, UITableViewDelegate, UITableViewDataSource {
+    var dropDownOptions = [String]()
+    
+    var tableView = UITableView()
+    
+    var delegate : dropDownProtocol!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        tableView.backgroundColor = UIColor.darkGray
+        self.backgroundColor = UIColor.darkGray
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.addSubview(tableView)
+        
+        tableView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dropDownOptions.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.text = dropDownOptions[indexPath.row]
+        cell.backgroundColor = UIColor.darkGray
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.delegate.dropDownPressed(string: dropDownOptions[indexPath.row])
+        self.tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
+    
+}
+
+
+}
+
