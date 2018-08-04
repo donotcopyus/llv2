bd273def0321c1fa718a965a521c8a25a935781d
crazydiamond <lunacao214@gmail.com>
2018-08-03 23:05:36 -0400

    多图上传完成

--- llv2/sendAd/xianzhiVController.swift
+++ llv2/sendAd/xianzhiVController.swift
@@ -7,21 +7,70 @@
 //
 
 import UIKit
+import ImagePicker
 
-class xianzhiVController: UIViewController {
+class xianzhiVController: UIViewController,UITextViewDelegate,ImagePickerDelegate {
+    
+    func wrapperDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        
+    }
+    
+    func doneButtonDidPress(_ imagePicker: ImagePickerController, images: [UIImage]) {
+        
+        //用户只上传了一张图片
+        if (images.count == 1){
+            self.image2.image = images[0]
+            self.image.image = nil
+            self.image3.image = nil
+        }
+            //两张
+        else if (images.count == 2){
+             self.image2.image = nil
+            self.image.image = images[0]
+            self.image3.image = images[1]
+        }
+            //三张
+        else if(images.count == 3){
+        self.image.image = images[0]
+        self.image2.image = images[1]
+        self.image3.image = images[2]
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func cancelButtonDidPress(_ imagePicker: ImagePickerController) {
+     imagePicker.dismiss(animated:true,completion:nil)
+    }
+    
     
     
     @IBOutlet weak var image: UIImageView!
     
+    @IBOutlet weak var image2: UIImageView!
+    
+    @IBOutlet weak var image3: UIImageView!
+    
     @IBOutlet weak var name: UITextField!
     
     @IBOutlet weak var price: UITextField!
+
+    @IBOutlet weak var txtv: UITextView!
     
-    @IBOutlet weak var text: UITextView!
+    //placeholder
+  
     
     
     @IBAction func upload(_ sender: Any) {
+        
+        let imagePickerController = ImagePickerController()
+        imagePickerController.delegate = self
+        imagePickerController.imageLimit = 3
+        present(imagePickerController,animated: true, completion: nil)
+        
+        
     }
+    
+    
     
     @IBAction func handle(_ sender: Any) {
     }
@@ -32,7 +81,13 @@ class xianzhiVController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+          txtv.text = "在这里填写详细信息"
+          txtv.textColor = UIColor.lightGray
+          txtv.returnKeyType = .done
+          txtv.delegate = self
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -40,7 +95,20 @@ class xianzhiVController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "在这里填写详细信息"{
+            textView.text = ""
+            textView.textColor = UIColor.black
+        }
+    }
 
 
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n"{
+            textView.resignFirstResponder()
+        }
+        return true
+    }
 
 }
