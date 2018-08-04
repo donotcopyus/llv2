109cf336f6762c4d277cfd2f779d505abefc4727
crazydiamond <lunacao214@gmail.com>
2018-08-03 21:55:14 -0400

    edit

---
+++ llv2/sendAd/xianzhiVController.swift
@@ -0,0 +1,46 @@
+//
+//  xianzhiVController.swift
+//  llv2
+//
+//  Created by Luna Cao on 2018/8/3.
+//  Copyright © 2018年 Luna Cao. All rights reserved.
+//
+
+import UIKit
+
+class xianzhiVController: UIViewController {
+    
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var price: UITextField!
+    
+    @IBOutlet weak var text: UITextView!
+    
+    
+    @IBAction func upload(_ sender: Any) {
+    }
+    
+    @IBAction func handle(_ sender: Any) {
+    }
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
